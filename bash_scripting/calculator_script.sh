1f1a316fcc2a9d0704f8794b4cdf26e17357e6c7
Enamrab <enamrab9@gmail.com>
2026-04-24 11:36:27 +0530

    new commit

--- bash_scripting/calculator_script.sh
+++ bash_scripting/calculator_script.sh
@@ -2,7 +2,7 @@
 
 #Author: Enam Rab
 #Date Created: 22/04
-#Last Modified: 22/04
+#Last Modified: 23/04
 
 #Description:
 #This script demonstarte the usage of Positional Parameters by making simple calculator program
@@ -19,7 +19,15 @@ echo
 echo "Enter the operator e.g.+/-/*: $1"
 echo "Entered number1: $2"
 echo "Entered number2: $3"
-echo $((${2:-0} $1 ${3:-0}))
+echo "Special parameter demo $* "$*" $@ "$@" "
+echo
+
+echo $((${2:-0} $1 ${3:-0} $1 ${4:-0} $1 ${5:-0}))
+for var in "$@+1"; do
+	echo "$var"
+	result=$((result $1 $var))
+done
+echo $result
 
 if [[ "$1" == "+" ]]; then
 	result+=$((${3} + ${2}))
@@ -31,7 +39,6 @@ elif [[ "$1" == "*" ]]; then
 else
 	echo "You entered wrong operator choice"
 fi
-
 echo "You performed ${operator} on $3 and $2: Your Result is ${result}"
 
 exit 0
---
+++ bash_scripting/special.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Author: Enam Rab
+#Date created: 24/04
+#Last modified: 24/04
+
+#Description
+#This script uses special parameter concept to perform mathematical calculations
+#Inputs will be given by command line
+
+#Usage
+#use of special parameter in calculator program
+
+echo $@
+echo $(($@))
+
+
